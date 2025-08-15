033c84b6c76780c54b7c676d71d3edc122889211
George Finch <georgefinch12@gmail.com>
2025-08-15 17:26:34 +0100

    yii2 and firebase now communicate.

--- basic/controllers/GameController.php
+++ basic/controllers/GameController.php
@@ -4,13 +4,27 @@ namespace app\controllers;
 use yii\rest\Controller;
 use app\models\Game;
 use yii\filters\auth\HttpBasicAuth;
+use Kreait\Firebase\Factory;
+
 
 class GameController extends Controller
 {
     public function actionIndex()
     {
-        return $output = new Game();
-    }
+      
+        $factory = (new Factory)->withServiceAccount("../config/firebase_credentials.json");
+        $factory = $factory->withDatabaseUri((new \app\models\DBURL)->URL);
+        $database = $factory->createDatabase();
+
+
+         $snapshot = $database->getReference('/BoardGame/ticketToRide');
+
+         return $snapshot->getValue();
+        
+      
+       
+        
+        }
   
 
 
